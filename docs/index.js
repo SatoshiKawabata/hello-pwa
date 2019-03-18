0d1ce93a636e0a4bc9028199934547ddfdae31f5
Satoshi Kawabata <satoshi.kawabata@nulab-inc.com>
2019-03-18 16:18:15 +0900

    init

---
+++ docs/index.js
@@ -0,0 +1,9 @@
+console.log("hello-pwa");
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./service_worker.js').then(function(registration) {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }).catch(function(err) {
+        console.log('ServiceWorker registration failed: ', err);
+    });
+}
---
+++ docs/service_worker.js
@@ -0,0 +1,29 @@
+// キャッシュファイルの指定
+var CACHE_NAME = 'hello-pwa-caches';
+var urlsToCache = [
+    '/satoshikawabata.github.io/',
+];
+
+// インストール処理
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(function(cache) {
+                console.log("installed sw", event, cache);
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// リソースフェッチ時のキャッシュロード処理
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(function(response) {
+                console.log("installed sw", event, response);
+                return response ? response : fetch(event.request);
+            })
+    );
+});
